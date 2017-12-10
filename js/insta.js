600448d08fbff758907b9a1240f990ece29a4733
eloirodriguezgaxas <erodriguezg@edu.tecnocampus.cat>
2017-12-10 08:39:32 +0100

    Image gallery from instagram

--- js/insta.js
+++ js/insta.js
@@ -1,39 +1,58 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    var insta = new Instafeed ({
-    get: 'user',
-    userId: '623597756',
-    clientId: 'Client',
-    accessToken: 'Access-Token',
-    resolution: 'standard_resolution',
-    sortBy: 'most-recent',
-    limit: 9,
-    template: '<div class="image-item insta-page">' +
-     '<a class="image insta-image" href="{{image}}>' +
-     '<img alt="{{user.full_name}}" src="{{image}}>' +
-     '<div class="img-backdrop-tutorial">' +
-     '<div class="insta-caption">' +
-     '<p>{{caption}}</p>' +
-     '</div>' +
-     '</div>' +
-     '<div class="captions">' +
-     '<a></a>' +
-     '</div>' +
-     '</a>' +
-     '</div>',
-         
-    filter: function(image) {
-     return image.type === 'image';
-    },
-    
+document.addEventListener("DOMContentLoaded", function () {
+  /*
+    var insta = new Instafeed({
+      get: 'user',
+      userId: '2708246576',
+      clientId: '801566096fac4a2c9774644caea82ae4',
+      accessToken: '2708246576.8015660.730b0378941a4bd6a5c0f7e30aee912a',
+      resolution: 'standard_resolution',
+      sortBy: 'most-recent',
+      useHttp: "true",
+      limit: 6,
+      template: '<div class="col-xs-12 col-sm-6 col-md-4"><a href="{{image}}"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p><span class="likes"><i class="icon ion-heart"></i> {{likes}}</span><span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span></div><img src="{{image}}" class="img-responsive"></div></a></div>',
+      sortBy: 'most-recent',
+  
+      filter: function (image) {
+        return image.type === 'image';
+      },
+  
+      after: function () {
+        loader.style.display = 'none';
+        if (!this.hasNext()) {
+          loadButton.setAttribute('disabled', 'disabled');
+        }
+      }
+    });
+  
+    insta.run();
+  */
+  var galleryFeed = new Instafeed({
+    get: "user",
+    userId: 2708246576,
+    accessToken: "2708246576.8015660.730b0378941a4bd6a5c0f7e30aee912a",
+    resolution: "standard_resolution",
+    limit: 6,
+   /* template: '<div class="gallery">' +
+                '<a target="_blank" href="{{link}}">' +
+                  '<img src="{{image}}" alt="Trolltunga Norway" width="300" height="200">' +
+                '</a>' +
+                '<div class="desc"><p>{{caption}}</p></div>' +
+              '</div>'
+              ,*/
+    template: '<div class="col-xs-12 col-sm-6 col-md-4"><a href="{{link}}" target="_blank"><div class="img-featured-container"><div class="img-backdrop"></div><div class="description-container"><p class="caption">{{caption}}</p></div><img src="{{image}}" class="img-responsive"></div></a></div>',
+    target: "instafeed-gallery-feed",
     after: function () {
-     loader.style.display = 'none';
-       if (!this.hasNext()) {
-         loadButton.setAttribute('disabled', 'disabled');
-       }
-     }
-   });
-     
-   insta.run();
-   
-   });
+      // disable button if no more results to load
+      if (!this.hasNext()) {
+        btnInstafeedLoad.setAttribute('disabled', 'disabled');
+      }
+    },
+  });
+  galleryFeed.run();
+
+  var btnInstafeedLoad = document.getElementById("btn-instafeed-load");
+  btnInstafeedLoad.addEventListener("click", function () {
+    galleryFeed.next()
+  });
+
+});
